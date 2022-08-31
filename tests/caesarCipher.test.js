52087c8751163bfda978e06bcab7df7dcefa715e
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-31 22:28:30 +0100

    Added additional tests

--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -7,3 +7,11 @@ test('Encrypts a string using caesar cipher', () => {
 test('Encrypts a string using caesar cipher and a key', () => {
     expect(caesarCipher('abcdefghijklmnopqrstuvwxyz', 3)).toBe('defghijklmnopqrstuvwxyzabc');
 });
+
+test('Encrypts a string that has punctuation using caesar cipher', () => {
+    expect(caesarCipher('abcdef,ghijklm?nopqr,stuvwxyz!')).toBe('bcdefg,hijklmn?opqrs,tuvwxyza!');
+});
+
+test('Encrypts a string that has capital letters using caesar cipher', () => {
+    expect(caesarCipher('abcdEfghijklMnopqrStuvwxyZ')).toBe('bcdefghijklmnopqrstuvwxyza');
+});
